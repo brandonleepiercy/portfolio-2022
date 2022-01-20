1ef2e8758396f9669c87502e2f91f5e969f3baa3
Brandon Lee Piercy <b.piercy567@gmail.com>
2022-01-20 12:42:34 -0800

    added logo and responsiveness

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,32 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Nav.css';
 import { Link } from 'react-router-dom';
 
 function Nav() {
 
-    return (
-      <div className='nav-container'>
-          <div className='nav-wrapper'>
-              <div className='nav-button'>
+  let [atHome, setAtHome] = useState(true);
+
+  const handleClick = (homeOrNot) => {
+    if (homeOrNot == true) {
+      setAtHome(true);
+    } else if (homeOrNot == false) {
+      setAtHome(false);
+    };
+  };
+
+  return (
+    <div>
+      {atHome ? <div></div> : <div className='nav-initials-logo'>BP</div>}
+      <div className={atHome ? 'nav-container' : 'nav-container-away'}>
+          <div className={atHome ? 'nav-wrapper' : 'nav-wrapper-away'}>
+              <div className='nav-button' onClick={atHome ? () => console.log('Already at home.'): () => handleClick(true)}>
                 <Link to="/">Home</Link>
               </div>
               <div className='nav-button'>
-                <Link to="/about">About Me</Link>
+                <Link onClick={atHome? () => handleClick(false) : console.log()} to="/about">About Me</Link>
               </div>
               <div className='nav-button'>
-                <Link to="/projects">Projects</Link>
+                <Link onClick={atHome? () => handleClick(false) : console.log()} to="/projects">Projects</Link>
               </div>
               <div className='nav-button'>
-                <Link to="/contact">Contact</Link>
+                <Link onClick={atHome? () => handleClick(false) : console.log()} to="/contact">Contact</Link>
               </div>
               <div className='nav-button'>
-                <Link to="/resume">Resume</Link>
+                <Link onClick={atHome? () => handleClick(false) : console.log()} to="/resume">Resume</Link>
               </div>                                                        
           </div>
-          <div className='nav-line'>
+          <div className={atHome ? 'nav-line' : 'nav-line-away'}>
           </div>
       </div>
+    </div>
   );
-}
+};
 
 export default Nav;
