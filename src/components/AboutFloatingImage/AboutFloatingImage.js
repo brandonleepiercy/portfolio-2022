41b90111c5ce3f79495a01d6e1f04cf613ccb846
Brandon Lee Piercy <b.piercy567@gmail.com>
2022-01-24 13:59:55 -0800

    crunch has begun

--- src/components/AboutFloatingImage/AboutFloatingImage.js
+++ src/components/AboutFloatingImage/AboutFloatingImage.js
@@ -13,7 +13,7 @@ function AboutFloatingImage() {
           </div>
           <div className='floating-image-wrapper'>
               <img className='floating-image' src={floatingImage} 
-              alt='a red image of me servicing one of the spark plugs on my Cayenne. 
+              alt='Me servicing one of the spark plugs on my Cayenne. 
               I appear extra frustrated because I snapped the head off the prior coil pack bolt. 
               I believe the raw display of emotion adds to the artistic nature of the webpage overall.'/>
           </div>
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,25 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ContactForm.css';
 
+
 function ContactForm() {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (name === null || name.length < 2) {
+            alert('Please provide a valid name.');
+        } else if (email === null || emailRegex.test(email) === false) {
+            alert('Please provide a valid email.');
+        } else if (subject === null || subject.length < 2) {
+            alert('Please add a subject');
+        } else if (message === null ||message.length < 1) {
+            alert('Please add a message.');
+        } else if (name !== null && email !== null && subject !== null && message !== null) {
+            console.log('successful submission attempt');
+        } else {
+            console.log('Some type of error occured. Please refresh.');
+        }
+    };
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [subject, setSubject] = useState("");
+    const [message, setMessage] = useState("");
+
+    const emailRegex = /[^\s@]+@[^\s@]+\.[^\s@]+/;
+
     return (
         <div className='contact-form-container'>
             <form>
                 <div className='contact-first-row'>
-                    <input className='contact-name-input' type='text' id='contact-name' name='name' placeholder='Name' required/>
-                    <input className='contact-email-input' type='text' id='contact-email' name='email' placeholder='Email' required/>                 
+                    <input className='contact-name-input' type='text' id='contact-name' name='name' placeholder='Name' autoComplete='off' onChange={(event) => {setName(event.target.value)}} required/>
+                    <input className='contact-email-input' type='email' id='contact-email' name='email' placeholder='Email' autoComplete='off' onChange={(event) => {setEmail(event.target.value)}} required/>                 
                 </div>
 
                 <div className='contact-second-row'>
-                    <input className='contact-subject-input' type='text' id='contact-subject' name='subject' placeholder='Subject' required/>
+                    <input className='contact-subject-input' type='text' id='contact-subject' name='subject' placeholder='Subject' autoComplete='off' onChange={(event) => {setSubject(event.target.value)}} required/>
                 </div>
 
                 <div className='contact-third-row'>
-                    <textarea className='contact-message-input' id='contact-message' name='message' required/>
+                    <textarea className='contact-message-input' id='contact-message' name='message' onChange={(event) => {setMessage(event.target.value)}} required/>
                 </div>
 
                 <div className='contact-submit-container'>
-                    <button className='contact-submit-button'>SEND</button>
+                    <button className='contact-submit-button' onClick={(event) => {handleSubmit(event)}}>SEND</button>
                 </div>
             </form>
         </div>
