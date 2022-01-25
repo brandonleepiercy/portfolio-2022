ebf2c2a6c5ebcd4c56655d11336bb8f73e0212ce
Brandon Lee Piercy <b.piercy567@gmail.com>
2022-01-24 19:00:26 -0800

    a lot of things were completed

---
+++ src/components/ProjectsBox/PBScreenRow/PBScreenRow.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { HiOutlineFolder } from 'react-icons/hi';
+
+function PBScreenRow(props) {
+
+    let { title } = props;
+    let { size } = props;
+    let { id } = props;
+    let { setSelected } = props;
+    let { setHome } = props;
+
+    return (
+        <div className='screen-home-project-row' value={id} key={id} id={id} onClick={(event) => {
+            setSelected(event.target.id);
+            setHome(false);
+        }}>
+            <div className='screen-project-icon-wrapper'>
+            <HiOutlineFolder color={'#879777'} size={'2vw'} id={id}/>
+            </div>
+
+            <div className='screen-project-title-wrapper' id={id}>
+            {title}
+            </div>
+
+            <div className='screen-project-size-wrapper' id={id}>
+            {size}
+            </div>
+        </div>
+    );
+};
+
+export default PBScreenRow;
