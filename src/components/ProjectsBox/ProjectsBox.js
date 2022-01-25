d4f3dd5541feef1cc00720b3bb67760c07d8a3f9
Brandon Lee Piercy <b.piercy567@gmail.com>
2022-01-25 09:11:11 -0800

    bug fixes complete

--- src/components/ProjectsBox/ProjectsBox.js
+++ src/components/ProjectsBox/ProjectsBox.js
@@ -14,7 +14,7 @@ const projects = [
     public: true,
     githubLink: 'https://github.com/brandonleepiercy/portfolio-2022',
     deployment: 'https://brandon.im/',
-    img: 'https://hfmibiza.com/wp-content/uploads/2021/02/oliver-heldens.jpg',
+    img: 'https://i.imgur.com/Ur1WnFb.png',
     description: 'My revised portfolio for 2022. Done in quite a hurry and still evolving but I am much happier with it now than it was. Tried to focus on pleasing colors and modern 3-dimensonal effects. Designed and developed myself.',
     technology: 'React.js, Node.js, Express, Nodemailer, AWS Elastic Beanstalk, Amplify, AWS-SDK and SES'
   },
@@ -23,8 +23,8 @@ const projects = [
     title: 'java-gradle-springboot-backend',
     size: '83 KB',
     public: false,
-    img: 'https://hfmibiza.com/wp-content/uploads/2021/02/oliver-heldens.jpg',
-    description: 'Internal backend system ported from AWS Lambdas/DynamoDB to a Java/Gradle/Spring Boot backend hosted on AWS Elastic Beanstalk, via AWS CodePipeline & AWS RDS, developed in a team of 3.',
+    img: 'https://i.imgur.com/GS62PDv.png',
+    description: 'Internal backend system ported from AWS Lambdas/DynamoDB to a Java/Gradle/Spring Boot backend hosted on AWS Elastic Beanstalk, via AWS CodePipeline & AWS RDS, developed in a team of 3. The Cognizant AWS account that this is hosted on is not public.',
     technology: 'Java, Gradle, Spring Boot, AWS Elastic Beanstalk, CodePipeline, RDS, Cognito'
   },
   {
@@ -32,8 +32,8 @@ const projects = [
     title: 'react-jest-social-media-site',
     size: '10.32 MB',
     public: false,
-    img: 'https://hfmibiza.com/wp-content/uploads/2021/02/oliver-heldens.jpg',
-    description: 'Picture sharing social media site developed in a 4 week cycle with 4 pair programmers. Responsible for coding front-end components and testing, as well as making calls to the back-end to route information. Also responsible for delivering presentations to relevant stakeholders at the end of sprints.',
+    img: 'https://i.imgur.com/inEUFED.png',
+    description: 'Picture sharing social media site developed in a 4 week cycle with 4 pair programmers. Responsible for coding front-end components and testing, as well as making calls to the back-end to route information. Also responsible for delivering presentations to relevant stakeholders at the end of sprints. The Cognizant AWS account that this is hosted on is not public.',
     technology: 'React.js, Node.js, Jest, Amplify, auth0'
   },
   {
@@ -41,8 +41,8 @@ const projects = [
     title: 'auto-parts-e-commerce-site',
     size: '7.92 MB',
     public: false,
-    img: 'https://hfmibiza.com/wp-content/uploads/2021/02/oliver-heldens.jpg',
-    description: 'MVP turn key auto-parts e-commerce store designed and developed as a part of an internal onboarding training program with Cognizant. Front end built with Typescript, React.js, and Node.js. Backend build on AWS Dynamo DB, routes handled with AWS Lambdas. Authentication with AWS Cognito and payment with Stripe. Deployed AWS accounts shut down, seeking to re-deploy.',
+    img: 'https://i.imgur.com/inEUFED.png',
+    description: 'MVP turn key auto-parts e-commerce store designed and developed as a part of an internal onboarding training program with Cognizant. Front end built with Typescript, React.js, and Node.js. Backend build on AWS Dynamo DB, routes handled with AWS Lambdas. Authentication with AWS Cognito and payment with Stripe. The Cognizant AWS account that this is hosted on is not public.',
     technology: 'Typescript, React.js, Node.js, AWS Lambdas, AWS DynamoDB, AWS Cognito, Stripe' 
   },
   {
@@ -52,7 +52,7 @@ const projects = [
     public: true,
     githubLink: 'https://github.com/ma-wong/project-3',
     deployment: 'http://snippet.ltd/',
-    img: 'https://hfmibiza.com/wp-content/uploads/2021/02/oliver-heldens.jpg',
+    img: 'https://i.imgur.com/lSeXwC0.png',
     description: 'Final project for the coding bootcamp I completed in January of 2020. Part of a team of 5, handled building front-end components as well as building out all of the routing for all of the various data models to the SQL backend. Front end on AWS Amplify, backend on RDS.',
     technology: 'React.js, Node.js, SQL' 
   }
